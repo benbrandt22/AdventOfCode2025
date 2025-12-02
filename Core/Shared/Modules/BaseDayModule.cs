87ecfd018c296d3d4e239feedb653a8ce8f6fecb
Ben Brandt <benbrandt22@gmail.com>
2025-12-02 01:11:03 -0600

    fixed test output

--- Core/Shared/Modules/BaseDayModule.cs
+++ Core/Shared/Modules/BaseDayModule.cs
@@ -3,10 +3,12 @@
 public abstract class BaseDayModule : IDayModule
 {
     private InputDataProvider _inputDataProvider;
+    private readonly ITestOutputHelper _outputHelper;
 
     public BaseDayModule(ITestOutputHelper outputHelper)
     {
         _inputDataProvider = new InputDataProvider();
+        _outputHelper = outputHelper;
 
         // the test class gets instantiated once per test, so we can output the header here to make it display at the start of each unit test
         OutputHeader();
@@ -24,22 +26,7 @@ public abstract class BaseDayModule : IDayModule
     public abstract string Title { get; }
     
     protected string GetData(InputType inputType) => _inputDataProvider.GetInputData(Year, Day, inputType);
-    protected string GetData(string inputType) => _inputDataProvider.GetInputData(Year, Day, inputType);
-
-    /// <summary>
-    /// When set to true, calls to Debug() will be written to the output.
-    /// </summary>
-    public static bool DebugEnabled { get; set; }
-    
-    internal void WriteLine(string line = "") => Console.WriteLine(line);
-
-    internal void Debug(string line = "")
-    {
-        if (DebugEnabled)
-        {
-            System.Diagnostics.Debug.WriteLine(line);
-        }
-    }
-
+    protected string GetData(string inputType) => _inputDataProvider.GetInputData(Year, Day, inputType);   
+    internal void WriteLine(string line = "") => _outputHelper.WriteLine(line);
     internal void WriteHorizontalRule(int length = 80) => WriteLine(new string('-', length));
 }
