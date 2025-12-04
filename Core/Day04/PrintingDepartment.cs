31f462f0500c18a756fa12e8750be8c8ead45f80
Ben Brandt <benbrandt22@gmail.com>
2025-12-04 00:36:19 -0600

    Day 4 solution

---
+++ Core/Day04/PrintingDepartment.cs
@@ -0,0 +1,88 @@
+﻿using System.Diagnostics;
+using Core.Shared;
+using Core.Shared.Extensions;
+using Core.Shared.Modules;
+
+namespace Core.Day04;
+
+public class PrintingDepartment : BaseDayModule
+{
+    public PrintingDepartment(ITestOutputHelper outputHelper) : base(outputHelper) { }
+    
+    public override int Day => 4;
+
+    public override string Title => "Printing Department";
+
+    [Fact] public void Part1_Sample() => ExecutePart1(GetData(InputType.Sample)).ShouldBe(13);
+    [Fact] public void Part1() => ExecutePart1(GetData(InputType.Input));
+
+    [Fact] public void Part2_Sample() => ExecutePart2(GetData(InputType.Sample)).ShouldBe(43);
+    [Fact] public void Part2() => ExecutePart2(GetData(InputType.Input));
+
+    public long ExecutePart1(string data)
+    {
+        var floorMap = LoadGrid(data);
+
+        var solution = GetAccessiblePaperLocations(floorMap).Count();
+        WriteLine($"Solution: {solution}");
+        return solution;
+    }
+    
+    public long ExecutePart2(string data)
+    {
+        var floorMap = LoadGrid(data);
+
+        var removablePaperLocationCount = 0;
+        while (true)
+        {
+            var accessiblePaperLocations = GetAccessiblePaperLocations(floorMap).ToList();
+            if(accessiblePaperLocations.Count == 0)
+            {
+                break;
+            }
+
+            removablePaperLocationCount += accessiblePaperLocations.Count;
+
+            RemovePaper(floorMap, accessiblePaperLocations);
+        }
+
+        var solution = removablePaperLocationCount;
+        WriteLine($"Solution: {solution}");
+        return solution;
+    }
+
+    private Grid<char> LoadGrid(string inputText)
+    {
+        var grid = new Grid<char>(inputText.ToGrid(removeEmptyLines: true));
+        WriteLine($"Loaded grid of {grid.RowCount} rows and {grid.ColumnCount} columns");
+        return grid;
+    }
+
+    private IEnumerable<GridCoordinate> GetAccessiblePaperLocations(Grid<char> floorMap)
+    {
+        bool hasPaper(GridCoordinate x) => x.IsInBounds(floorMap) && floorMap[x] == '@';
+
+        foreach (var coord in floorMap.AllCoordinates())
+        {
+            if (hasPaper(coord) && GetAllNeighborCoordinates(coord).Count(hasPaper) < 4)
+            {
+                yield return coord;
+            }
+        }
+    }
+
+    private IEnumerable<GridCoordinate> GetAllNeighborCoordinates(GridCoordinate coordinate)
+    {
+        return GridDirection.AllDirections.Select(d => coordinate.Move(d));
+    }
+
+    private void RemovePaper(Grid<char> floorMap, IEnumerable<GridCoordinate> locationsToClear)
+    {
+        foreach (var coord in locationsToClear)
+        {
+            floorMap[coord] = '.';
+        }
+    }
+
+}
+
---
+++ Core/Shared/Grid.cs
@@ -0,0 +1,99 @@
+﻿using System.Diagnostics;
+using System.Text;
+using Core.Shared.Extensions;
+
+namespace Core.Shared;
+
+/// <summary>
+/// A basic grid structure based on a 2D array of rows & columns, with the 0,0 origin in the top left corner
+/// </summary>
+public class Grid<T>
+{
+    private readonly T[,] _array;
+
+    public Grid(T[,] array)
+    {
+        _array = array;
+    }
+    
+    public int RowCount => _array.GetLength(0);
+    public int ColumnCount => _array.GetLength(1);
+    
+    public T ValueAt(GridCoordinate coordinate) => _array[coordinate.Row, coordinate.Column];
+    
+    public T this[int row, int column]
+    {
+        get => _array[row, column];
+        set => _array[row, column] = value;
+    }
+    
+    public T this[GridCoordinate coordinate]
+    {
+        get => _array[coordinate.Row, coordinate.Column];
+        set => _array[coordinate.Row, coordinate.Column] = value;
+    }
+
+    public IEnumerable<GridCoordinate> AllCoordinates()
+    {
+        var totalRows = RowCount;
+        var totalColumns = ColumnCount;
+        for (var row = 0; row < totalRows; row++)
+        {
+            for (var column = 0; column < totalColumns; column++)
+            {
+                yield return new GridCoordinate(row, column);
+            }
+        }
+    }
+}
+
+[DebuggerDisplay("(r{Row}, c{Column})")]
+public record GridCoordinate(int Row, int Column)
+{
+    public GridCoordinate Move(GridDirection direction, int distance = 1) =>
+        new(Row + (direction.DRow * distance), Column + (direction.DCol * distance));
+        
+    public bool IsInBounds<T>(Grid<T> grid)
+    {
+        return Row >= 0 && Row < grid.RowCount && Column >= 0 && Column < grid.ColumnCount;
+    }
+}
+
+public record GridDirection(int DRow, int DCol, GridDirection.DirectionName Name)
+{
+    public static readonly GridDirection Up = new(-1, 0, DirectionName.Up);
+    public static readonly GridDirection Down = new(1, 0, DirectionName.Down);
+    public static readonly GridDirection Left = new(0, -1, DirectionName.Left);
+    public static readonly GridDirection Right = new(0, 1, DirectionName.Right);
+    public static readonly GridDirection UpLeft = new(-1, -1, DirectionName.UpLeft);
+    public static readonly GridDirection UpRight = new(-1, 1, DirectionName.UpRight);
+    public static readonly GridDirection DownLeft = new(1, -1, DirectionName.DownLeft);
+    public static readonly GridDirection DownRight = new(1, 1, DirectionName.DownRight);
+
+    public static readonly IReadOnlyList<GridDirection> AllDirections = new[]
+        { Up, Down, Left, Right, UpLeft, UpRight, DownLeft, DownRight };
+    
+    public enum DirectionName
+    {
+        Up, Down, Left, Right,
+        UpLeft, UpRight, DownLeft, DownRight
+    }
+}
+
+public static class GridExtensions
+{
+    public static string Visualize<T>(this Grid<T> grid, Func<T, string> visualizer)
+    {
+        var lines = new List<string>();
+        for (int row = 0; row < grid.RowCount; row++)
+        {
+            var line = Enumerable.Range(0, grid.ColumnCount)
+                .Select(i => visualizer(grid[row, i]))
+                .JoinWith("");
+            lines.Add(line);
+        }
+        return string.Join(Environment.NewLine, lines);
+    }
+
+    public static string Visualize(this Grid<char> grid) => grid.Visualize(c => c.ToString());
+}
