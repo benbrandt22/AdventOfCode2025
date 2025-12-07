ce09f034dbc442609ac0e5b07f7b539fcfd6b334
Ben Brandt <benbrandt22@gmail.com>
2025-12-07 13:03:36 -0600

    Day 6 refactored & completed

--- Core/Day06/TrashCompactor.cs
+++ Core/Day06/TrashCompactor.cs
@@ -16,59 +16,87 @@ public class TrashCompactor : BaseDayModule
     [Fact] public void Part1_Sample() => ExecutePart1(GetData(InputType.Sample)).ShouldBe(4277556);
     [Fact] public void Part1() => ExecutePart1(GetData(InputType.Input));
 
-    [Fact(Skip = "Not yet implemented")] public void Part2_Sample() => ExecutePart2(GetData(InputType.Sample)).ShouldBe(-1);
-    [Fact(Skip = "Not yet implemented")] public void Part2() => ExecutePart2(GetData(InputType.Input));
+    [Fact] public void Part2_Sample() => ExecutePart2(GetData(InputType.Sample)).ShouldBe(3263827);
+    [Fact] public void Part2() => ExecutePart2(GetData(InputType.Input));
 
     public long ExecutePart1(string data)
     {
-        var (rows, operators) = LoadWorksheet(data);
+        var rawProblems = LoadWorksheetProblems(data);
 
-
-        var problemSolutions = new List<long>();
-
-        for (int i = 0; i < operators.Count; i++)
-        {
-            var problemNumbers = rows.Select(r => r[i]).ToList();
-            
-            var problemSolution = operators[i] switch
-            {
-                "+" => problemNumbers.Sum(),
-                "*" => problemNumbers.Aggregate(1L, (acc, val) => acc * val),
-                _ => throw new InvalidOperationException($"Unknown operator {operators[i]}"),
-            };
-
-            problemSolutions.Add(problemSolution);
-        }
-
-
-        var solution = problemSolutions.Sum();
+        var solution = rawProblems.Sum(p => p.SolveAsHuman());
         WriteLine($"Solution: {solution}");
         return solution;
     }
     
     public long ExecutePart2(string data)
     {
-        var solution = 0;
+        var rawProblems = LoadWorksheetProblems(data);
+
+        var solution = rawProblems.Sum(p => p.SolveAsCephalopod());
         WriteLine($"Solution: {solution}");
         return solution;
     }
 
-    private (List<List<long>> rows, List<string> operators) LoadWorksheet(string data)
+    private IEnumerable<RawMathProblem> LoadWorksheetProblems(string data)
     {
         var lines = data.ToLines(removeEmptyLines: true);
-        var rows = new List<List<long>>();
-        var operators = new List<string>();
 
-        for(int i = 0; i < (lines.Count-1); i++)
+        var operatorLine = lines.Last();
+
+        // position of each operator lines up with the start of each problem column
+        var problemStartIndexes = operatorLine.AllIndexesOf("+", StringComparison.OrdinalIgnoreCase)
+            .Concat(lines.Last().AllIndexesOf("*", StringComparison.OrdinalIgnoreCase))
+            .Order().ToList();
+
+        for (int problemId = 0; problemId < problemStartIndexes.Count; problemId++)
         {
-            var row = lines[i].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(x => long.Parse(x)).ToList();
-            rows.Add(row);
+            var startIndex = problemStartIndexes[problemId];
+            var endIndex = (problemId + 1 < problemStartIndexes.Count)
+                ? (problemStartIndexes[problemId + 1] - 2)
+                : (operatorLine.Length - 1);
+
+            var numberBlock = lines
+                .Take(lines.Count - 1)
+                .Select(line => line[startIndex..(endIndex + 1)])
+                .ToList();
+
+            var operatorChar = operatorLine[startIndex];
+
+            yield return new RawMathProblem(numberBlock, operatorChar);
+        }
+    }
+
+    public record RawMathProblem(List<string> NumberTextLines, char Operator)
+    {
+        /// <summary>
+        /// Reads horizontal numbers top to bottom
+        /// </summary>
+        public long SolveAsHuman() {
+            var humanNumbers = NumberTextLines.Select(x => long.Parse(x));
+            return Solve(humanNumbers);
         }
 
-        operators = lines.Last().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
+        /// <summary>
+        /// Reads vertical numbers right to left
+        /// </summary>
+        public long SolveAsCephalopod() {
+            var columnCount = NumberTextLines.First().Length;
+            var cephalopodNumbers = new List<long>();
+            for (int columnIndex = (columnCount-1); columnIndex >= 0; columnIndex--)
+            {
+                var numberString = new string(NumberTextLines.Select(l => l[columnIndex]).ToArray());
+                cephalopodNumbers.Add(long.Parse(numberString));
+            }
+            return Solve(cephalopodNumbers);
+        }
 
-        return (rows, operators);
-    }
+        private long Solve(IEnumerable<long> Numbers) => Operator switch
+        {
+            '+' => Numbers.Sum(),
+            '*' => Numbers.Aggregate(1L, (acc, val) => acc * val),
+            _ => throw new InvalidOperationException($"Unknown operator {Operator}"),
+        };
+    };
 
 }
 
